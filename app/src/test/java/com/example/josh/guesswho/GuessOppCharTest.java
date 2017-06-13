4dc5ab08897f650e18d52e3777b72b54d3489693
Josh Brauner <braunerj120007@gmail.com>
2017-06-13 09:18:06 -0600

    Create GuessOppCharTest.java

---
+++ app/src/test/java/com/example/josh/guesswho/GuessOppCharTest.java
@@ -0,0 +1,81 @@
+/**
+ * testSuite:
+ * This java class contains the full suite of test methods pertaining to the "Guess Who?" app.
+ *
+ * @Date June 12, 2017
+ * @Author Josh Brauner, Kyle Jorgensen, Evan Loughlin
+ */
+
+package com.example.josh.guesswho;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class GuessOppCharTest {
+    
+    private int[] testArray = new int[24];
+    private Player compPlayer = new Player();
+    private Player humPlayer = new Player();
+    private Game game = new Game();
+    
+    @before
+    public void before() {
+        compPlayer.setChar("becca");
+    }
+    
+    @after
+    public void after() {
+        testArray = new int[24];
+    }
+
+    /** Test 1: testAttributeInquiryMatches
+    * Test to ensure that the question asked by the user correctly matches the question
+    * which is implemented in the program's logic.
+    */
+    @Test
+    public void testAttributeInquiryMatches() throws Exception {
+        assertArrayEquals(testArray, game.getOppCharArray());
+    }
+
+    /** Test 2: testGuessCharacterCorrectly
+    * Test to see if game results in win if user (player) guesses character correctly.
+    * This should test whether the logic of the game matches correctly when the player guesses
+    * their opponent's character. The game should end, resulting in a win for the player.
+    *
+    */
+    @Test
+    public void testGuessCharacterCorrectly() throws Exception {
+        assertArrayEquals(testArray, game.getOppCharArray());
+    }
+
+    /** Test 3: testGuessCharacterIncorrectly
+    * Test case, to ensure that the program's response correctly matches expectations when a player
+    * guesses the opponent's character incorrectly. Specifically, the asking player's character
+    * selection should be reduced by one.
+    *
+    */
+    @Test
+    public void testGuessCharacterIncorrectly() throws Exception {
+        assertArrayEquals(testArray, game.getOppCharArray());
+    }
+
+    /** Test 4: testInquireAttribute
+    * Test case, whereby the the results of the guess are tested to match the correct
+    * response of the program's logic. For instance: if a user guesses whether their opponent's
+    * character has black hair and the answer is "No", then following the inquiry, that player's
+    * array of possible characters should not contain any character with black hair.
+    */
+    @Test
+    public void testInquireAttribute() throws Exception {
+        assertArrayEquals(testArray, game.getOppCharArray());
+    }
+
+    /** Test 5: testWinIfOneRemaining
+    * Test case to ensure that any player which has only one possible character remaining will
+    * automatically win the game.
+    *
+    */
+    @Test
+    public void testWinIfOneRemaining() throws Exception {
+        assertEquals(True, game.humHasWon());
+    }
+}

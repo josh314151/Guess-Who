6b2581fae1ed16985e0459ca5f4eeb808aef40c8
Josh Brauner <braunerj120007@gmail.com>
2017-06-13 21:36:57 -0600

    made test class

---
+++ app/src/main/java/com/example/josh/guesswho/Character.java
@@ -0,0 +1,30 @@
+package com.example.josh.guesswho;
+
+/**
+ * Created by Josh on 2017-06-13.
+ */
+
+public class Character {
+    private String name;
+    private boolean[][] attributes;
+
+    public Character(String name, int type, int value) {
+
+    }
+
+    public void setAttributes(int type, int value) {
+
+    }
+
+    public void setName(String s) {
+
+    }
+
+    public boolean[][] getAttributes() {
+        return attributes;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ app/src/main/java/com/example/josh/guesswho/Inquiry.java
@@ -0,0 +1,30 @@
+package com.example.josh.guesswho;
+
+/**
+ * Created by Josh on 2017-06-13.
+ */
+
+public class Inquiry {
+    private int lookup;
+    private String question;
+
+    public Inquiry(int lookup, String question) {
+
+    }
+
+    public void setLookup(int i) {
+
+    }
+
+    public void setQuestion(String s) {
+
+    }
+
+    public int getLookup() {
+        return lookup;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+}
